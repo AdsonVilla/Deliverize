2ab0da2e614ebed729570f57ee93c05d2c02b535
AdsonVilla <adson23_@hotmail.com>
2023-06-04 10:55:33 -0300

    Componentizar o Popover

--- src/Components/OrderAddInfos/index.jsx
+++ src/Components/OrderAddInfos/index.jsx
@@ -1,8 +1,8 @@
 import "./OrderAddInfos.css";
 import { IoIosAdd, IoIosRemove } from "react-icons/io";
 import { ExtraItems } from "../ExtraItems";
-import { Popover, Typography, List, ListItem, AlertTitle } from "@mui/material";
 import { useState, useEffect } from "react";
+import { PopoverComponent } from "../Popover";
 
 export function OrderAddInfos() {
   const [productInfos, setProductInfos] = useState([]);
@@ -33,7 +33,6 @@ export function OrderAddInfos() {
       .then((response) => response.json())
       .then((data) => setProductInfos(data[0].ingredients[0].itens));
   }, []);
-  //   console.log(productInfos[0].nm_item);
 
   return (
     <div className="oderAddInfos__wrapper">
@@ -59,23 +58,24 @@ export function OrderAddInfos() {
           </div>
           <div className="cutlery__confirmation">
             <div className="confirmation__tag">
-              <label for="Yes">Sim</label>
+              <label htmlFor="Yes">Sim</label>
               <input
                 type="radio"
                 id="Yes"
                 name="Yes"
                 value={true}
-                style={{ borderColor: "#FEBC10" }}
+                style={{ borderColor: "1px solid #FEBC10" }}
               />
             </div>
             <div className="confirmation__tag">
-              <label>Não</label>
+              <label htmlFor="No">Não</label>
+
               <input
                 type="radio"
                 id="No"
                 name="No"
                 value={false}
-                style={{ borderColor: "#FEBC10" }}
+                style={{ border: 0 }}
               />
             </div>
           </div>
@@ -93,42 +93,19 @@ export function OrderAddInfos() {
             />
             <span>{quantity}</span>
             <IoIosAdd
-              style={{ color: "#ED3237", width: "14px", height: "14px" }}
+              style={{ color: "#ED3237", width: "16px", height: "16px" }}
               onClick={handleIncrementQuantity}
             />
           </div>
           <button onClick={handleAddToCart}>Adicionar</button>
 
-          <Popover
+          <PopoverComponent
             open={Boolean(anchorEl)}
             anchorEl={anchorEl}
             onClose={() => setAnchorEl(null)}
             anchorReference="anchorPosition"
             anchorPosition={{ top: -1, left: 1050 }}
-          >
-            <Typography sx={{ p: 2, bgcolor: "#F09035", color: "#FFFF" }}>
-              Adicionado com sucesso!
-            </Typography>
-            <List sx={{ p: 2 }}>
-              <AlertTitle
-                sx={{
-                  color: "#BC2E31",
-                  fontWeight: "700",
-                }}
-              >
-                Oferta Cheddar Bacon
-              </AlertTitle>
-              <AlertTitle sx={{ color: "#4E4E4E" }}>
-                Ingredientes:
-                <List>
-                  <ListItem>1 Carne 250gr</ListItem>
-                  <ListItem>2 Queijo Cheddar</ListItem>
-                  <ListItem>1 Bacon</ListItem>
-                  <ListItem>Molho Especial</ListItem>
-                </List>
-              </AlertTitle>
-            </List>
-          </Popover>
+          />
         </div>
       </div>
     </div>
--- src/Components/Popover/index.jsx
+++ src/Components/Popover/index.jsx
@@ -1,9 +1,42 @@
-import "./Popover.css";
+import { Popover, Typography, List, ListItem, AlertTitle } from "@mui/material";
 
-export function Popover() {
+export function PopoverComponent({
+  open,
+  anchorEl,
+  onClose,
+  anchorReference,
+  anchorPosition,
+}) {
   return (
-    <div>
-      <h1>Popover</h1>
-    </div>
+    <Popover
+      open={Boolean(anchorEl)}
+      anchorEl={anchorEl}
+      onClose={() => setAnchorEl(null)}
+      anchorReference="anchorPosition"
+      anchorPosition={{ top: -1, left: 1050 }}
+    >
+      <Typography sx={{ p: 2, bgcolor: "#F09035", color: "#FFFF" }}>
+        Adicionado com sucesso!
+      </Typography>
+      <List sx={{ p: 2 }}>
+        <AlertTitle
+          sx={{
+            color: "#BC2E31",
+            fontWeight: "700",
+          }}
+        >
+          Oferta Cheddar Bacon
+        </AlertTitle>
+        <AlertTitle sx={{ color: "#4E4E4E" }}>
+          Ingredientes:
+          <List>
+            <ListItem>1 Carne 250gr</ListItem>
+            <ListItem>2 Queijo Cheddar</ListItem>
+            <ListItem>1 Bacon</ListItem>
+            <ListItem>Molho Especial</ListItem>
+          </List>
+        </AlertTitle>
+      </List>
+    </Popover>
   );
 }
